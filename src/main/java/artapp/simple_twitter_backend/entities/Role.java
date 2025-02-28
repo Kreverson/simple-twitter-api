a69fbbb01c679a7ec10ee6003230a298fc31caac
Kreverson <kreversonjulho@gmail.com>
2025-02-28 19:20:00 -0300

    Criado as entidades, relacionamentos e configurações do JPA

---
+++ src/main/java/artapp/simple_twitter_backend/entities/Role.java
@@ -0,0 +1,50 @@
+package artapp.simple_twitter_backend.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public enum Values {
+        BASIC(2L),
+        ADMIN (1L);
+
+        Long roleId;
+
+        Values(Long roleId) {
+            this.roleId = roleId;
+        }
+
+        public Long getRoleId() {
+            return roleId;
+        }
+
+        public void setRoleId(Long roleId) {
+            this.roleId = roleId;
+        }
+    }
+}
