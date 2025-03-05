edbc1d4308eee217472e38e4c2270c4db94b85fa
Kreverson <kreversonjulho@gmail.com>
2025-03-05 12:33:05 -0300

    Criado funcionalidade delete tweet somente do usuário logado e proprietário do tweet ou admin

--- src/main/java/artapp/simple_twitter_backend/controller/TweetController.java
+++ src/main/java/artapp/simple_twitter_backend/controller/TweetController.java
@@ -1,14 +1,15 @@
 package artapp.simple_twitter_backend.controller;
 
 import artapp.simple_twitter_backend.controller.dto.CreateTweetDto;
+import artapp.simple_twitter_backend.entities.Role;
 import artapp.simple_twitter_backend.entities.Tweet;
 import artapp.simple_twitter_backend.repository.TweetRepository;
 import artapp.simple_twitter_backend.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -34,6 +35,25 @@ public class TweetController {
 
         tweetRepository.save(tweet);
 
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,
+                                            JwtAuthenticationToken token) {
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+
+        var tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var isAdmin = user.get().getRoles()
+                .stream().anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+        if (isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
+            tweetRepository.deleteById(tweetId);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
         return ResponseEntity.ok().build();
     }
 }
